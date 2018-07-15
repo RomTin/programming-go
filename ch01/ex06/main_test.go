fc14d99ddd9fb447a750f7fd0e5d4329a1a85e38
RomTin <micfall.romtin@gmail.com>
2018-07-15 21:17:23 +0900

    add ex06

---
+++ ch01/ex06/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+	"math/rand"
+	"bytes"
+	"time"
+	"image/gif"
+	"image/color"
+)
+
+func TestMain(t *testing.T) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	for i := 0; i < 100; i++ {
+		palette = append(palette, color.RGBA{uint8(rand.Int63() % 255), uint8(rand.Int63() % 255), uint8(rand.Int63() % 255), 0xFF})
+	}	
+	gif_buffer := new(bytes.Buffer)
+	lissajous(gif_buffer)
+	r := bytes.NewReader(gif_buffer.Bytes())
+	_, err := gif.Decode(r)
+	if err != nil {
+		t.Errorf("something went wrong in generating GIF")
+	}
+}
