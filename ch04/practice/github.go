8257955a27b9f560c5ddfc85c3da39b20ffa1d7a
Ryoya Komatsu <micryo@r.recruit.co.jp>
2018-08-16 19:55:13 +0900

    wip

---
+++ ch04/practice/github.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"time"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const IssuesURL = "https://api.github.com/search/issues"
+
+type IssuesSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items	[]*Issue
+}
+
+type Issue struct {
+	Number int
+	HTMLURL string `json:"html_url"`
+	Title string
+	State string
+	User *User
+	CreatedAt time.Time `json:"created_at"`
+	Body string
+}
+
+type User struct {
+	Login string
+	HTMLURL string `json:"html_url"`
+}
+
+func SearchIssues()
